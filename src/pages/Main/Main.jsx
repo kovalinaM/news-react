383fde85b5ebc216ca95866c471177a45a18ce89
Kovaleva Marina <kirienko1995@mail.ru>
2024-11-08 15:29:45 +0300

    add skeleton for new news and banner

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,17 +1,21 @@
 import styles from './styles.module.css';
 import NewsBanner from "../../components/NewsBanner/NewsBanner.jsx";
 import NewsList from "../../components/NewsList/NewsList.jsx";
+import Skeleton from "../../components/Skeleton/Skeleton.jsx";
 import {useEffect, useState} from "react";
 import {getNews} from "../../api/apiNews.js";
 
 const Main = () => {
     const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchNews = async () => {
             try {
+                setIsLoading(true);
                 const response = await getNews();
                 setNews(response.news);
+                setIsLoading(false);
             } catch (error) {
                 console.log(error);
             }
@@ -21,9 +25,17 @@ const Main = () => {
 
     return (
         <main className={styles.main}>
-            {news.length > 0 ? <NewsBanner item={news[0]}/> : null}
+            {news.length > 0 && !isLoading ? (
+                <NewsBanner item={news[0]}/>
+            ) : (
+                <Skeleton count={1} type={'banner'}/>
+            )}
 
-            <NewsList news={news}/>
+            {!isLoading ? (
+                <NewsList news={news}/>
+            ) : (
+                <Skeleton count={10} type={'item'}/>
+            )}
         </main>
     )
 }
