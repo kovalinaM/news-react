4cc21058ea1f2ce221329ec2d568cdda42b91b31
Kovaleva Marina <kirienko1995@mail.ru>
2025-01-14 12:48:20 +0300

    Add design for web + refactoring

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,19 +1,29 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
-const Categories = ({categories, setSelectedCategory, selectedCategory}) => {
+const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
     return (
         <div className={styles.categories}>
-            {categories.map(category => {
+            <button
+                onClick={() => setSelectedCategory(null)}
+                className={!selectedCategory ? styles.active : styles.item}
+            >
+                All
+            </button>
+            {categories.map((category) => {
                 return (
                     <button
                         onClick={() => setSelectedCategory(category)}
-                        className={selectedCategory === category ? styles.active : styles.item}
-                        key={category}>
+                        className={
+                            selectedCategory === category ? styles.active : styles.item
+                        }
+                        key={category}
+                    >
                         {category}
                     </button>
-                )
+                );
             })}
         </div>
-    )
-}
-export default Categories
+    );
+};
+
+export default Categories;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,97 +1,15 @@
-import styles from './styles.module.css';
-import NewsBanner from "../../components/NewsBanner/NewsBanner.jsx";
-import NewsList from "../../components/NewsList/NewsList.jsx";
-import Pagination from "../../components/Pagination/Pagination.jsx";
-import {useEffect, useState} from "react";
-import { getNews} from "../../api/apiNews.js";
-import dataCategories from "../../utils/data.js";
-import Categories from "../../components/Categories/Categories.jsx";
-import Search from "../../components/Search/Search.jsx";
-import {useDebounce} from "../../helpers/hooks/useDebounce.js";
-import {TOTAL_PAGES, PAGE_SIZE} from "../../constants/constants.js";
+import styles from "./styles.module.css";
+import LatestNews from "../../components/LatestNews/LatestNews";
+import NewsByFilters from "../../components/NewsByFilters/NewsByFilters";
 
 const Main = () => {
-    const [news, setNews] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [categories, setCategories] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState('All');
-    const [keywords, setKeywords] = useState('');
-
-    const debouncedKeywords = useDebounce(keywords, 1500);
-
-    const fetchNews = async (currentPage) => {
-        try {
-            setIsLoading(true);
-            const response = await getNews({
-                page_number: currentPage,
-                page_size: PAGE_SIZE,
-                category: selectedCategory === 'All' ? null : selectedCategory,
-                keywords: debouncedKeywords,
-            });
-            setNews(response.news);
-            setIsLoading(false);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const fetchCategories = () => {
-        setCategories(['All', ...dataCategories.categories]);
-    }
-
-    useEffect(() => {
-            fetchCategories()
-        },
-        []);
-
-
-    useEffect(() => {
-            fetchNews(currentPage);
-        }, [currentPage, selectedCategory, debouncedKeywords]);
-
-    const handleNextPage = () => {
-        if (currentPage < TOTAL_PAGES) {
-            setCurrentPage(currentPage + 1);
-        }
-    }
-
-    const handlePreviousPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    }
-
-    const handlePageClick = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    }
-
     return (
         <main className={styles.main}>
-            <Categories categories={categories} setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory} />
+            <LatestNews />
 
-            <Search keywords={keywords} setKeywords={setKeywords} />
-
-            <NewsBanner isLoading={isLoading} item={news && news[0]}/>
-
-            <Pagination
-                handleNextPage={handleNextPage}
-                handlePreviousPage={handlePreviousPage}
-                handlePageClick={handlePageClick}
-                totalPages={TOTAL_PAGES}
-                currentPage={currentPage}
-            />
-
-            <NewsList isLoading={isLoading} news={news}/>
-
-            <Pagination
-                handleNextPage={handleNextPage}
-                handlePreviousPage={handlePreviousPage}
-                handlePageClick={handlePageClick}
-                totalPages={TOTAL_PAGES}
-                currentPage={currentPage}
-            />
+            <NewsByFilters />
         </main>
-    )
-}
+    );
+};
+
 export default Main;
